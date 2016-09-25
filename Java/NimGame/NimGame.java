293a7e4f07e7791d60ee894a32133762b9fbfcd1
Phoenix-Z <phoenixzhao1105@gmail.com>
2016-09-25 17:19:49 +0800

    nim game.

---
+++ Java/NimGame/NimGame.java
@@ -0,0 +1,15 @@
+/**
+ * (1ms)
+ * 这就是一个智力题。。。
+ * @author Phoenix-Z
+ *
+ */
+public class NimGame {
+
+	public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+	
+	public static void main(String[] args) {
+	}
+}
