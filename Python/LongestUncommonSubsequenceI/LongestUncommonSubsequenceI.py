c276f4b498afedca783e482a2f55354ebb060dab
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-04-06 13:06:48 +0800

    solutions to longest uncommon subsequece I in python.

---
+++ Python/LongestUncommonSubsequenceI/LongestUncommonSubsequenceI.py
@@ -0,0 +1,9 @@
+class Solution(object):
+	# (39ms)
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        return max(len(a), len(b)) if a != b else -1;
