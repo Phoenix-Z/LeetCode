7b4de59b34255d79b7b8de17e617e44c47cc90f6
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-04-25 15:50:16 +0800

    solution to array partition I in python.

---
+++ Python/ArrayPartitionI/ArrayPartitionI.py
@@ -0,0 +1,8 @@
+class Solution(object):
+	# (125ms)
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(sorted(nums)[::2])
