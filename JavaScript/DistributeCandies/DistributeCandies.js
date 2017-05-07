777ac60c547c56b6b121cd38830486ea404b897b
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-05-07 22:34:09 +0800

    solution to distribute candies in javascript.

---
+++ JavaScript/DistributeCandies/DistributeCandies.js
@@ -0,0 +1,8 @@
+/**
+ * (356ms)
+ * @param {number[]} candies
+ * @return {number}
+ */
+var distributeCandies = function(candies) {
+    return Math.min(candies.length / 2, new Set(candies).size);
+};
