013072d372911a6e02cb31fe881afde5f70596ff
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-05-07 22:34:23 +0800

    solution to distribute candies in python.

---
+++ Python/DistributeCandies/DistributeCandies.py
@@ -0,0 +1,8 @@
+class Solution(object):
+	# (176ms)
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        return min(len(set(candies)), len(candies) / 2)
