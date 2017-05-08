af812e429dcd8cb948c82f817f2dd5d274da2b44
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-05-08 15:41:14 +0800

    solution to subtree of another tree in javascript.

---
+++ JavaScript/SubtreeOfAnotherTree/SubtreeOfAnotherTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * (192ms)
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+    var isSame = function(s, t) {
+        if(s === null && t === null)
+            return true;
+        if(s === null || t === null)
+            return false;
+        if(s.val != t.val)
+            return false;
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    };
+    if(s === null)
+        return false;
+    return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);        
+};
