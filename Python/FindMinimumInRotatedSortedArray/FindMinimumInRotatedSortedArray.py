544c34f732d078bdb0965995a38bb0217f95590f
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-05-09 12:28:57 +0800

    solution to find minimum rotated sorted array in python.

---
+++ Python/FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+	# (52ms)
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) / 2
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[right]
+        
