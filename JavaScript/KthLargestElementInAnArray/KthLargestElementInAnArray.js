1ebb0388ba79c3b76f3c26faed0321e44ca2579a
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-07-01 21:20:44 +0800

    solution to kth largest element in an array in javascript.

---
+++ JavaScript/KthLargestElementInAnArray/KthLargestElementInAnArray.js
@@ -0,0 +1,10 @@
+/**
+ * (122ms)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort((a, b) => b - a);
+    return nums[k - 1];
+};
