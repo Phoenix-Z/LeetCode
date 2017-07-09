af4034da3d20590a41690e04360389790c3d2437
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-07-09 18:48:59 +0800

    solution to elimination game in javascript.

---
+++ JavaScript/EliminationGame/EliminationGame.js
@@ -0,0 +1,10 @@
+/**
+ * (172ms)
+ * @param {number} n
+ * @return {number}
+ */
+var lastRemaining = function(n) {
+    return n === 1 ? 1 : 2 * (1 + Math.floor(n/2) - lastRemaining(Math.floor(n/2)));
+};
+
+console.log(lastRemaining(9));
